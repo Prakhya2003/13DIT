038f18c96011a9808c1defc1bdd81eab67315c26
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-06-12 20:52:33 +1200

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,38 +1,37 @@
 <?php
 
-include "config.php"; // Using database connection file here
+include "config.php"; // Connect to the database
 
-$id = (isset($_GET['id']) ? $_GET['id'] : '');
+$id = (isset($_GET['id']) ? $_GET['id'] : ''); // get id through through link
 
-$sql = "select * from user where ID='$id'";
+$qry = mysqli_query($conn,"select * from user where ID='$id'"); // select query based on the ID
 
-$result = $conn->query($sql);
+$data = mysqli_fetch_array($qry); // fetch data
 
-if ($result->num_rows > 0){
-
-$row = $result->fetch_assoc();
-
-$username = $row["username"];
-$email = $row["email"];
-
-echo
-
-"<html>
-<body>
-
-<form action='phpUpdateFormScript.php' method='post'>
-Username: $username<br>
-Username: <input type='text' name='name' value='$username'><br>
-Email: <input type='text' name='age' value='$email'><br>
-<input type ='submit'>
-</form>
-
-</body>
-</html>";
-
-} else {
-	echo "Not Found";
+if(isset($_POST['update'])) // when click on Update button
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+	
+    $edit = mysqli_query($conn,"update user set username='$username', email='$email' where ID='$id'");
+	
+    if($edit) //if edit has worked
+    {
+        mysqli_close($conn); // Close connection
+        header("location:logout.php"); // redirects to My Profile page after changed
+        exit;
+    }
+    else 
+    {
+        echo mysqli_error($conn); //if the edit has not worked show error
+    }    	
 }
-$conn->close();
-
 ?>
+
+<h3>Update Data</h3>
+
+<form method="POST"> 
+  <input type="text" name="username" value="<?php echo $data['username'] ?>" placeholder="Enter New Username"> //Change Username
+  <input type="text" name="email" value="<?php echo $data['email'] ?>" placeholder="Enter New Email"> // Change email
+  <input type="submit" name="update" value="Update"> // commit changes
+</form>
