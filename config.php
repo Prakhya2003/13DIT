e362b682beb32f07148fd890b90c4b4228d3c308
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-08-19 12:16:32 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php 
 
 $server = "localhost";
-$user = "root";
-$pass = "";
-$database = "login_register_pure_coding";
+$username = "id16960265_username_login_register";
+$password = "ssd~E=gCKP|J4YU~";
+$database = "id16960265_login_register";
 
-$conn = mysqli_connect($server, $user, $pass, $database);
+$conn = mysqli_connect($server, $username, $password, $database);
 
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
 }
 
 ?>
+
