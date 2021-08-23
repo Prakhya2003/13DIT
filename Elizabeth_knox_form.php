2bf0b33eb6fd3f65b497c345989a48da70f2dfcb
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-08-23 18:27:37 +1200

    Update Elizabeth_knox_form.php
    column name changed from username to user_id

--- Elizabeth_knox_form.php
+++ Elizabeth_knox_form.php
@@ -7,23 +7,23 @@ include 'config.php'; // establish connection with database
 error_reporting(0); 
 
 if (isset($_POST['submit'])) {        // If all okay, Add record with filled out data
-	$username = $_POST['username'];
+	$user_id = $_POST['user_id'];
 	$date = $_POST['date'];
 	$activity = $_POST['activity'];
 	$phone = $_POST['phone'];
 	$suburb = $_POST['suburb'];
 
 		if (!$result->num_rows > 0) {    // If form is filled, add records
-			$sql = "INSERT INTO sign_ups (username, date, activity, phone, suburb)
-					VALUES ('$username', '$date', '$activity', '$phone', '$suburb')";
+			$sql = "INSERT INTO sign_ups (user_id, date, activity, phone, suburb)
+					VALUES ('$user_id', '$date', '$activity', '$phone', '$suburb')";
 			$result = mysqli_query($conn, $sql);
 			if ($result) {       // On successfull completion
 				echo "<script>alert('Volunteering Sign Up Complete. You will be contacted directly by the organisation for further details and confirmation.')</script>";
-				$_POST['username'] = "";
-				$_POST['date'] = "";
-				$_POST['activity'] = "";
-				$_POST['phone'] = "";
-				$_POST['suburb'] = "";
+				$user_id = "";
+				$date = "";
+				$activity = "";
+				$phone = "";
+				$suburb = "";
 			} else {
 				echo "<script>alert('Woops! Something Went Wrong.')</script>";
 			}
@@ -48,7 +48,7 @@ if (isset($_POST['submit'])) {        // If all okay, Add record with filled out
 		<form action="" method="POST" class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800;">Volunteering Sign Up</p>
 			<div class="input-group">
-				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+				<input type="text" placeholder="Your Username" name="user_id" value="<?php echo $user_id; ?>" required>
 			</div>
 			<div class="input-group">
 				<input type="date" placeholder="Chosen Date" name="date" value="<?php echo $date; ?>" required>
