342b1e6692ebdce5c038fec11dc6d9dbf4bbd757
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-08-23 18:31:10 +1200

    Update opportunities.php
    Changed column name from username to user_id. 
    Added extra content and formating

--- opportunities.php
+++ opportunities.php
@@ -64,25 +64,26 @@ for (i = 0; i < acc.length; i++) {
     
         <?php
             session_start();
-            if(!isset($_SESSION['username']))
+            if(!isset($_SESSION['user_id']))
             {
         ?>
     
     <div class="item1"> 
-        <p> If you would like to sign up to any of the volunteering opportunities, you are required to <a href="login.php">Login</a>. <br> If you have not previously Registered, please do so here; <a href="register.php">Register</a>. </p>
+        <p> If you would like to sign up to any of the volunteering opportunities, you are required to <a href="login.php">Login</a>. <br> If you have not previously Registered, please do so here; <a href="register.php">Register</a>. <br> <br> Please Note that all the information you provide on this sign up form will be shared with the administration for volunteering at this specific organisation. No other information (including registration) will be shared with the organisation. This entry will be recorded and displayed as a record of achivement and experience on the 'My Profile' page.</p>
     </div>
     
             <?php 
             }
             else
             {
-                include 'Elizabeth_knox_form.php'; // Show the form to sign up from
+                include 'Elizabeth_knox_form.php';
             }
             ?>
     
     <div class="item2">
         <h3> Below is the Casual Volunteering Schedule for Elizabeth Knox </h3>
         <iframe width="100%" height="500px"frameborder="0" scrolling="no" src="https://epsomgirls-my.sharepoint.com/personal/17027_eggs_school_nz/_layouts/15/Doc.aspx?sourcedoc={852e3996-b044-4b2b-ae31-cf31cb0afc94}&action=embedview&wdAllowInteractivity=False&ActiveCell='Weekly%20Voulnteer%20Requirements'!B2&Item='Weekly%20Voulnteer%20Requirements'!A1%3AL40&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True"></iframe>
+        <p> Alternatively, click this<a href="https://epsomgirls-my.sharepoint.com/:x:/g/personal/17027_eggs_school_nz/EZY5LoVEsCtLrjHPMcsK_JQBNms9Kwa0FTGSjhfV4H6mCQ?e=HJ8DCP"> Link </a>to open the timetable in another tab. </p>
     </div>
     
     <div class="item4">
