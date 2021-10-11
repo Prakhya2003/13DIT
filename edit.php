2319f67a742c26bd3055027b9005183bb92bc420
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-10-11 21:07:02 +1300

    Update edit.php
    Edit page as expected but does not edit the entries

--- edit.php
+++ edit.php
@@ -2,9 +2,9 @@
 
 include "config.php"; // Using database connection file here
 
-$id = (isset($_GET['id']) ? $_GET['id'] : ''); // get id through query string
+$user_id = (isset($_GET['user_id']) ? $_GET['user_id'] : ''); // get id through query string
 
-$qry = mysqli_query($conn,"select * from user where ID='$id'"); // select query
+$qry = mysqli_query($conn,"select * from user where user_id='$user_id'"); // select query
 
 $data = mysqli_fetch_array($qry); // fetch data
 
@@ -12,20 +12,23 @@ if(isset($_POST['update'])) // when click on Update button
 {
     echo "<script>alert('User Profile Successfully Updated')</script>";
     
-    $username = $_POST['username'];
+    $fullname = $_POST['fullname'];
     $email = $_POST['email'];
+    $DOB = $_POST['DOB'];
 	
-    $edit = mysqli_query($conn,"update user set username='$username', email='$email' where ID='$id'");
+    $edit = mysqli_query($conn,"update user set fullname='$fullname', email='$email', DOB='$DOB' where user_id='$user_id'");
 	
     if($edit)
     {
+        echo "<script>alert('User Profile Successfully Updated')</script>";
         mysqli_close($conn); // Close connection
         header("location:logout.php"); // logs out
         exit;
     }
     else
     {
-        echo mysqli_error($conn);
+            echo "<script>alert('Connection Failed')</script>";
+	    echo mysqli_error($conn);
     }    	
 }
 
@@ -42,13 +45,13 @@ if(isset($_POST['update'])) // when click on Update button
 <h3>Update Profile Details</h3>
 
 <form method="POST">
-  <input type="text" name="username" value="<?php echo $data['username'] ?>" placeholder="Enter New Username">
+  <input type="text" name="fullname" value="<?php echo $data['fullname'] ?>" placeholder="Enter New Full Name">
   <input type="text" name="email" value="<?php echo $data['email'] ?>" placeholder="Enter New Email">
   <input type="date" name="DOB" value="<?php echo $data['DOB'] ?>" placeholder="Enter New Date of Birth">
   <input type="submit" name="update" value="Update">
 </form>
-
-<p> Here you can edit your Profile Details incing your Name, Email and Date of Birth. <br><br> Once you have edited as per your wish, press the Update button. <br><br> 
+	    
+<p> Here you can edit your Profile Details including your Name, Email and Date of Birth. <br><br> Once you have edited as per your wish, press the Update button. <br><br> 
 The website will redirect you to the home page. Please Login with any updated details to continue browsing. 
 <br><br>
 Do you wish to go back without editing? <a href="index.php">Go Home</a> <br><br>
