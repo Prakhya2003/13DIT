807ed8d3747fbc296ecfb85d58a278ffc85a4e0f
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-10-14 13:19:05 +1300

    Update config.php
    Final with Commenting

--- config.php
+++ config.php
@@ -5,11 +5,12 @@ $username = "id16960265_username_login_register";
 $password = "ssd~E=gCKP|J4YU~";
 $database = "id16960265_login_register";
 
-$conn = mysqli_connect($server, $username, $password, $database);
+$conn = mysqli_connect($server, $username, $password, $database); // $conn is used to establish a connection with 
 
 if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
+    die("<script>alert('Connection Failed.')</script>"); // if connection is not established, show error 
 }
 
-?>
+// This code is included on every page where a connection to the database is required (common code)
 
+?>
