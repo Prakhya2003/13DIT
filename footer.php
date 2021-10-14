04c53c23bd11074b05a2eafbb1109f5463f1a9d4
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-10-14 14:42:03 +1300

    Update footer.php
    Final with Code Commenting

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-
-    <div class="item3">
+    <div class="item3"> <!-- Bottom strip for footer -->
         <p> &copy; Prakhya Mathur 2021 </p>
     </div>
     
 </div>
     
 </body>
-</html>
+</html> <!-- End of page code where common footer code is used -->
