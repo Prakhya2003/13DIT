f73ac935ce04b0f3ffdcd1c0cd3d750e6efd0549
Prakhya2003 <67714042+Prakhya2003@users.noreply.github.com>
2021-10-25 15:38:57 +1300

    Create search.php
    Search database

---
+++ search.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Search</title>
+    </head>
+
+<body>
+
+<form action="phpsearch.php" method="post">
+Search Activity: <input type="text" name="search"><br>
+<input type ="submit">
+</form>
+
+</body>
+</html>
